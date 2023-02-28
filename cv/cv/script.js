b445caf945cd1df2b9866734dc2de46b2d7d7731
howe23 <90195412+howe23@users.noreply.github.com>
2023-02-28 10:06:56 -0300

    Delete script.js

--- cv/cv/script.js
+++
@@ -1,6 +0,0 @@
-//  const skillsSection = document.querySelector('.skills-section');
-//  const skillsContent = document.querySelector('.skills-content');
-
-//  skillsSection.addEventListener('click', function() {
-//    skillsContent.style.display = (skillsContent.style.display === 'none') ? 'block' : 'none';
-//  });
